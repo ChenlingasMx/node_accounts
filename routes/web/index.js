31111be016683973a2c462b18708ff46585c02b4
ChenlingasMx <42308734@qq.com>
2024-01-18 15:34:45 +0800

    fix: 增加session

--- routes/web/index.js
+++ routes/web/index.js
@@ -4,27 +4,37 @@ var router = express.Router();
 //导入 moment
 const moment = require("moment");
 const AccountModel = require("../../models/AccountModel");
+//导入中间件检测登录
+const checkLoginMiddleware = require("../../middlewares/checkLoginMiddleware");
+
+//添加首页路由规则
+router.get('/', (req, res) => {
+  //重定向 /account
+  res.redirect('/account');
+})
 
 // 记账本列表
-router.get("/account", function (req, res, next) {
+router.get("/account", checkLoginMiddleware, function (req, res, next) {
   //获取所有的账单信息
-  AccountModel.find().sort({time: -1}).exec((err, data) => {
-    if(err){
-      res.status(500).send('读取失败~~~');
-      return;
-    }
-    //响应成功的提示
-    res.render('list', {accounts: data, moment: moment});
-  })
+  AccountModel.find()
+    .sort({ time: -1 })
+    .exec((err, data) => {
+      if (err) {
+        res.status(500).send("读取失败~~~");
+        return;
+      }
+      //响应成功的提示
+      res.render("list", { accounts: data, moment: moment });
+    });
 });
 
 // 添加记录
-router.get("/account/create", function (req, res, next) {
+router.get("/account/create", checkLoginMiddleware, function (req, res, next) {
   res.render("create");
 });
 
 // 新增记录
-router.post("/account", function (req, res, next) {
+router.post("/account", checkLoginMiddleware, function (req, res, next) {
   //插入数据库
   AccountModel.create(
     {
@@ -44,7 +54,7 @@ router.post("/account", function (req, res, next) {
 });
 
 //删除记录
-router.get("/account/:id", (req, res) => {
+router.get("/account/:id", checkLoginMiddleware, (req, res) => {
   //获取 params 的 id 参数
   let id = req.params.id;
   AccountModel.deleteOne({ _id: id }, (err, data) => {
